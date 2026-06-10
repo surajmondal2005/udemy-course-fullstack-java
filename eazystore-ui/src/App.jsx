2ece51b628d877b8bfa77ea3bf5eb3413e21c14c
surajmondal2005 <31240042@vupune.ac.in>
2026-06-10 07:59:41 +0530

    Implement React Router navigation with nested routes

--- eazystore-ui/src/App.jsx
+++ eazystore-ui/src/App.jsx
@@ -1,16 +1,17 @@
-import './App.css'
+import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./components/Home";
+import { Outlet } from "react-router-dom";
 
 function App() {
-    return (
-       <> 
-        <Header/>
-        <Home/>
-        <Footer/>
-       </> 
-    );
+  return (
+    <>
+      <Header />
+        <Outlet/>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
--- eazystore-ui/src/main.jsx
+++ eazystore-ui/src/main.jsx
@@ -1,10 +1,51 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import Cart from "./components/Cart";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </StrictMode>,
-)
+);

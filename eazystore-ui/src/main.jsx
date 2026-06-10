ceecc6fb30690478e101c4eb2834ad305335d785
surajmondal2005 <31240042@vupune.ac.in>
2026-06-10 15:38:42 +0530

    Modified About,cart,contact,login

--- eazystore-ui/src/main.jsx
+++ eazystore-ui/src/main.jsx
@@ -8,41 +8,58 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Login from "./components/Login";
 import Cart from "./components/Cart";
+import ErrorPage from "./components/ErrorPage.jsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+const routeDefinitions = createRoutesFromElements(
+  <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+    <Route index element={<Home />}  />
+    <Route path="/home" element={<Home />}  />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />}  />
+    <Route path="/login" element={<Login />} />
+    <Route path="/cart" element={<Cart />} />
+  </Route>,
+);
+const appRouter = createBrowserRouter(routeDefinitions);
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/home",
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-    ],
-  },
-]);
+// const appRouter = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage/>,
+//     children: [
+//       {
+//         index: true,
+//         element: <Home />,
+//       },
+//       {
+//         path: "/home",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/about",
+//         element: <About />,
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact />,
+//       },
+//       {
+//         path: "/login",
+//         element: <Login />,
+//       },
+//       {
+//         path: "/cart",
+//         element: <Cart />,
+//       },
+//     ],
+//   },
+// ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>

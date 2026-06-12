aa55c58bb5aaaac4c79ef36bdb863835dbb4296b
surajmondal2005 <31240042@vupune.ac.in>
2026-06-12 15:47:19 +0530

    Created Post Contact us rest api

---
+++ eazystore/src/main/java/com/eazybytes/eazystore/dto/ContactRequestDto.java
@@ -0,0 +1,17 @@
+package com.eazybytes.eazystore.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class ContactRequestDto {
+
+
+    private String name;
+    private String email;
+    private String mobileNumber;
+    private String message;
+
+}
--- eazystore/src/main/java/com/eazybytes/eazystore/entity/Contact.java
+++ eazystore/src/main/java/com/eazybytes/eazystore/entity/Contact.java
@@ -1,39 +1,46 @@
 package com.eazybytes.eazystore.entity;
 
-@lombok.Getter
-@lombok.Setter@jakarta.persistence.Entity
-@jakarta.persistence.Table(name = "contacts")
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "contacts")
 public class Contact {
-@jakarta.persistence.Id
-@jakarta.persistence.GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
-@jakarta.persistence.Column(name = "contact_id", nullable = false)
-private java.lang.Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contact_id", nullable = false)
+    private Long id;
 
-@jakarta.persistence.Column(name = "name", nullable = false, length = 100)
-private java.lang.String name;
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
 
-@jakarta.persistence.Column(name = "email", nullable = false, length = 100)
-private java.lang.String email;
+    @Column(name = "email", nullable = false, length = 100)
+    private String email;
 
-@jakarta.persistence.Column(name = "mobile_number", nullable = false, length = 15)
-private java.lang.String mobileNumber;
+    @Column(name = "mobile_number", nullable = false, length = 15)
+    private String mobileNumber;
 
-@jakarta.persistence.Column(name = "message", nullable = false, length = 500)
-private java.lang.String message;
+    @Column(name = "message", nullable = false, length = 500)
+    private String message;
 
-@org.hibernate.annotations.ColumnDefault("CURRENT_TIMESTAMP")
-@jakarta.persistence.Column(name = "created_at", nullable = false)
-private java.time.Instant createdAt;
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
 
-@jakarta.persistence.Column(name = "created_by", nullable = false, length = 20)
-private java.lang.String createdBy;
+    @Column(name = "created_by", nullable = false, length = 20)
+    private String createdBy;
 
-@jakarta.persistence.Column(name = "updated_at")
-private java.time.Instant updatedAt;
+    @Column(name = "updated_at")
+    private Instant updatedAt;
 
-@jakarta.persistence.Column(name = "updated_by", length = 20)
-private java.lang.String updatedBy;
-
+    @Column(name = "updated_by", length = 20)
+    private String updatedBy;
 
 
 }

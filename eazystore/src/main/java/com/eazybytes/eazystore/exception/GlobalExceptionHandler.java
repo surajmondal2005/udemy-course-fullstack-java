ba0add3bab68f8d62de7e2e879d8bd823c8966a7
Suraj Mondal <31240042@vupune.ac.in>
2026-08-18 02:31:33 +0530

    Implemented logging

--- eazystore/src/main/java/com/eazybytes/eazystore/exception/GlobalExceptionHandler.java
+++ eazystore/src/main/java/com/eazybytes/eazystore/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.eazybytes.eazystore.exception;
 import com.eazybytes.eazystore.dto.ErrorResponseDto;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -18,25 +19,32 @@ import java.util.Map;
 import java.util.Set;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest webRequest) {
+        log.error("An exception occured due to: {}", exception.getMessage());
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(webRequest.getDescription(false), HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage(), LocalDateTime.now());
         /*return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponseDto);*/
-        return new ResponseEntity<>(errorResponseDto,HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,String>> handleValidationExceptions(MethodArgumentNotValidException exception) {
-        Map<String,String> errors = new HashMap<>();
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        log.error("An exception occured due to: {}", exception.getMessage());
+        Map<String, String> errors = new HashMap<>();
         List<FieldError> fieldErrorList = exception.getBindingResult().getFieldErrors();
-        fieldErrorList.forEach(error->errors.put(error.getField(),error.getDefaultMessage()));
+        fieldErrorList.forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
         return ResponseEntity.badRequest().body(errors);
     }
+
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Map<String,String>> handleConstraintViolationExceptions(ConstraintViolationException exception) {
-        Map<String,String> errors = new HashMap<>();
+    public ResponseEntity<Map<String, String>> handleConstraintViolationExceptions(ConstraintViolationException exception) {
+        log.error("An exception occured due to: {}", exception.getMessage());
+
+        Map<String, String> errors = new HashMap<>();
         Set<ConstraintViolation<?>> constraintViolationSet = exception.getConstraintViolations();
-        constraintViolationSet.forEach(constraintViolation->errors.put(constraintViolation.getPropertyPath().toString(),constraintViolation.getMessage()));
+        constraintViolationSet.forEach(constraintViolation -> errors.put(constraintViolation.getPropertyPath().toString(), constraintViolation.getMessage()));
         return ResponseEntity.badRequest().body(errors);
     }
 }
